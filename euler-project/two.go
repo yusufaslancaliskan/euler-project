98abd01cd3de0c5da01653bc605671178645d632
ycaliskan <yusufaslancalskan@gmail.com>
2020-03-18 00:01:08 +0300

    find sum of even fibonacci numbers less than limit

--- euler-project/two.go
+++ euler-project/two.go
@@ -1,1 +1,13 @@
 package euler_project
+
+func findSumOfEvenFibonacciNumbersLessThan(limit int) int {
+	var i, b, sum int = 2, 1, 0
+	for i < limit {
+		if i%2 == 0 {
+			sum += i
+		}
+		i = i + b
+		b = i - b
+	}
+	return sum
+}
